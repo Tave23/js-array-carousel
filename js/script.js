0fccb09f1e4cee80cd1af5f49e0785e9c11173f9
Davide Tavella <tavella01@gmail.com>
2021-10-27 18:31:03 +0200

    un lavoro decisamente più guardabile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const imgMain = document.getElementsByClassName('image-main');
-const imgSide = document.getElementsByClassName('image-side');
+const imgSide = document.getElementsByClassName('aside-img');
+
 let contatore = 0;
+
 const down = document.getElementById('down');
 const up = document.getElementById('up');
 
@@ -10,10 +12,11 @@ console.log(down);
 up.addEventListener('click', function(){
     imgMain[contatore].classList.remove('active');
     imgSide[contatore].classList.remove('active2');
-    
+
     contatore--;
 
     if(contatore < 0) contatore = imgMain.length - 1;
+
     imgMain[contatore].classList.add('active');
     imgSide[contatore].classList.add('active2');
     console.log(imgMain);
@@ -26,6 +29,7 @@ down.addEventListener('click', function(){
     contatore++;
 
     if(contatore > imgMain.length - 1) contatore = 0;
+
     imgMain[contatore].classList.add('active');
     imgSide[contatore].classList.add('active2');
 })
