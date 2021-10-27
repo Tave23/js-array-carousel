066ffe4cf23051ba7cf5a4c0751cd63c93c48830
Davide Tavella <tavella01@gmail.com>
2021-10-27 16:17:25 +0200

    briciole di js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const up = document.getElementById('up');
+const down = document.getElementById('down');
+
+
+
+up.addEventListener("click", function(){ 
+   
+}); 
+
+
+
+down.addEventListener("click", function(){ 
+   
+}); 
